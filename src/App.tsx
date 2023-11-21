2bbdec9339081019acd8fa6ded8fd56574f78f5a
마현우 <mhw35689402@gmail.com>
2023-11-21 15:33:22 +0900

    feat: 로그인 처리 페이지 등록

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from './pages/Home/Home';
 import Onboarding from "./pages/Onboarding";
+import GoogleLogin from "./pages/GoogleLogin";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/oauth/login" element={<GoogleLogin />} />
       </Routes>
     </BrowserRouter>
   )
