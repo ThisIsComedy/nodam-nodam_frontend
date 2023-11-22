96f8a084c18c1d5422afce7bdf5789fea4d3b042
마현우 <mhw35689402@gmail.com>
2023-11-23 00:10:38 +0900

    chore: recoil 셋업

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,24 @@ import Record from './pages/Record/Record';
 import Ranking from './pages/Ranking/Ranking';
 import Profile from './pages/Profile/Profile';
 import NotFound from './pages/NotFound/NotFound';
+import { RecoilRoot } from "recoil";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/oauth/login" element={<GoogleLogin />} />
-        <Route path="/record" element={<Record />} />
-        <Route path="/ranking" element={<Ranking />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/oauth/login" element={<GoogleLogin />} />
+          <Route path="/record" element={<Record />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   )
 }
 
