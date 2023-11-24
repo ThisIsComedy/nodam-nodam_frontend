73dd0358649801938440f1c27adc8ba1a620bafd
baeian <ddoory1103@gmail.com>
2023-11-24 10:39:47 +0900

    feat: 재도전 바텀시트 퍼블리싱

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import font from "../../styles/font";
 import color from "../../styles/color";
@@ -11,8 +11,12 @@ import KnowHowBox from "../../components/KnowHowBox/KnowHowBox";
 import ThreeDaysCheckBox from "../../components/ThreeDaysCheckBox/ThreeDaysCheckBox";
 import GrassChart from "../../components/GrassChart/GrassChart";
 import RateBox from "../../components/RateBox/RateBox";
+import TwoButton from "../../components/atoms/TwoButton";
 
 const Home = () => {
+	const [isBottomSheetOpen, setBottomSheetOpen] = useState(false);
+	const BottomSheetBackground = useRef<any>();
+
 	useEffect(() => {
 		const accessToken = localStorage.getItem("accessToken");
 		const refreshToken = localStorage.getItem("refreshToken");
@@ -37,9 +41,33 @@ const Home = () => {
 						<RateBox title="3일 연속 금연 횟수" rate={90} type="회" />
 						<RateBox title="아낀 금액" rate={9000} type="원" />
 					</BoxContainer>
-					<RetryButton>금연 재도전하기</RetryButton>
+					<RetryButton onClick={() => setBottomSheetOpen(true)}>금연 재도전하기</RetryButton>
+					{
+						isBottomSheetOpen &&
+						<BottomSheetContainer ref={BottomSheetBackground} onClick={e => {
+							if (e.target === BottomSheetBackground.current) {
+								setBottomSheetOpen(false);
+							}
+						}}>
+							<BottomSheetContent>
+								<BottomSheetHeader>
+									<BottomSheetTitle>금연 재도전</BottomSheetTitle>
+									<CloseIcon src="/assets/global/close.svg" onClick={() => setBottomSheetOpen(false)} />
+								</BottomSheetHeader>
+								<BottomSheetDesc>
+									흡연하셨나요?<br />
+									괜찮아요, 다시 도전하면 돼요.
+								</BottomSheetDesc>
+								<TwoButton
+									large="재도전"
+									small="닫기"
+									// largeOnClick={}
+									smallOnClick={() => setBottomSheetOpen(false)} />
+							</BottomSheetContent>
+						</BottomSheetContainer>
+					}
 				</Section>
-				<Footer isGNB={true} page={1} />
+				<Footer isGNB={!isBottomSheetOpen} page={1} />
 			</Screen>
 		</Layout>
 	);
@@ -53,6 +81,7 @@ const Section = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	position: relative;
 `;
 
 const Header = styled.div`
@@ -78,4 +107,60 @@ const RetryButton = styled.button`
 	${font.H5};
 	border-radius: 16px;
 	margin-top: 57px;
+`;
+
+const BottomSheetContainer = styled.div`
+	display: flex;
+	position: absolute;
+	align-items: flex-end;
+	width: 390px;
+	height: 844px;
+	top: -47px;
+	border-radius: 49px;
+	background-color: rgba(0,0,0,0.6);
+`;
+
+const BottomSheetContent = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 12px 0 58px;
+	width: 100%;
+	height: 320px;
+	border-radius: 36px 36px 49px 49px;
+	background-color: ${color.white}
+`;
+
+const BottomSheetHeader = styled.div`
+	display: flex;
+	position: relative;
+	width: 100%;
+	height: 48px;
+	align-items: center;
+	justify-content: center;
+`;
+
+const BottomSheetTitle = styled.p`
+	${font.H5};
+	color: ${color.black};
+	text-align: center;
+`;
+
+const CloseIcon = styled.img`
+	position: absolute;
+	width: 24px;
+	height: 24px;
+	right: 20px;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+	cursor: pointer;
+`;
+
+const BottomSheetDesc = styled.p`
+${font.H4};
+	color: ${color.black};
+	text-align: center;
+	margin: 41px 0;
 `;
